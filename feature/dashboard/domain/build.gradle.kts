d195afa4573acc9b2c692ab25302c14d81b95498
Miguel Ángel Mialdea Monzo Dev <miguelmialdeamonzo@gmail.com>
2026-03-08 21:15:24 +0100

    Fix build changing version 35 to 36

--- feature/dashboard/domain/build.gradle.kts
+++ feature/dashboard/domain/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.miguelmialdea.expensetracker.feature.dashboard.domain"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
--- feature/filter/ui/build.gradle.kts
+++ feature/filter/ui/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.miguelmialdea.expensetracker.feature.filter.ui"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
