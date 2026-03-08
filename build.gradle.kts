ddff6209398beb4e62c74c51a0869ce3940a60f5
Miguel Ángel Mialdea Monzo Dev <miguelmialdeamonzo@gmail.com>
2026-03-08 20:23:44 +0100

    Configure detekt with professional rules and CI/CD ready

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,19 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        autoCorrect = true
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    }
+
+    dependencies {
+        "detektPlugins"("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
+    }
 }
